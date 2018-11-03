81db36d4d3c0971c0203f7cddebee5412b5f96ff
kin29 <kin29.com@gmail.com>
2018-11-03 18:24:07 +0900

    サンクスページ(thanks.php)

---
+++ thanks.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>stripe DE おかいもの</title>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+    <style>
+        html, body {
+            height: 100%;
+        }
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            display: table;
+            font-weight: 100;
+            font-family: 'Lato';
+        }
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
+        .title {
+            font-size: 50px;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <div class="title">「stripe DE おかいもの」<br>ご利用TEGEありがとうございました。</div>
+    </div>
+</div>
+</body>
+</html>
